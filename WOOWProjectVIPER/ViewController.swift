8961285b35e7e4a31262551098734d86ba771d4b
Andrei M <mosneaga8@gmail.com>
2022-07-25 09:35:32 +0300

    Initial Commit

---
+++ WOOWProjectVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WOOWProjectVIPER
+//
+//  Created by Andrei Mosneag on 25.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
